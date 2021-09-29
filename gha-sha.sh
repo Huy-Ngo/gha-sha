b2f75fa40e07c3ba18883380d8273150cbf56f41
Ngô Ngọc Đức Huy <huyngo@disroot.org>
2021-09-29 14:33:40 +0700

    Use $@ to list all files in glob

--- gha-sha.sh
+++ gha-sha.sh
@@ -1,11 +1,11 @@
 #!/bin/bash
 if [ "$RUNNER_OS" == "Linux" ]; then
-  sha256sum $1
+  sha256sum "$@"
 elif [ "$RUNNER_OS" == "Windows" ]; then
-  for file in $(find $1)
+  for file in $(find "$@")
   do
     certutil -hashfile $file SHA256
   done
 elif [ "$RUNNER_OS" == "macOS" ]; then
-  shasum -a 256 $1
+  shasum -a 256 "$@"
 fi
